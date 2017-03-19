4db2bcba569d6feec85562886f85b363a5d66ade
Emanuel Gull <egull@umich.edu>
2017-03-19 16:06:00 -0400

    lasso setup as in lasso example

--- src/SpM_Lasso.hpp
+++ src/SpM_Lasso.hpp
@@ -1,7 +1,35 @@
 #include"maxent_matrix_def.hpp"
 
 ///Class for performing LASSO fitting using ADMM
+///minimize \f[1/2*|| Ax - b ||_2^2 + \lambda || x ||_1 \f]
+
 class Lasso{
 public:
-  Lasso(){}
+  Lasso(const Eigen::MatrixXd &A, const Eigen::VectorXd &b):
+  lasso_max_iter_(1000),
+  lasso_abs_tol_(1.e-4),
+  lasso_rel_tol_(1.e-2),
+  A_(A),
+  b_(b),
+  n_(A.cols()),
+  m_(A.rows()){
+    if(b_.rows() !=m_) throw std::runtime_error("b should have size m");
+  }
+private:
+  ///Maximum number of LASSO iterations
+  const int lasso_max_iter_;
+  ///absolute convergence tolerance
+  const double lasso_abs_tol_;
+  ///relative convergence tolerance
+  const double lasso_rel_tol_;
+
+  ///reference to the matrix A
+  const Eigen::MatrixXd &A_;
+  ///reference to the vector b
+  const Eigen::VectorXd &b_;
+
+  ///number of rows of A
+  const int n_;
+  ///number of cols of A
+  const int m_;
 };
--- test/SpM_LassoTest.cpp
+++ test/SpM_LassoTest.cpp
@@ -20,19 +20,84 @@
 #include "../src/SpM_Lasso.hpp"
 #include "gtest.h"
 #include <fstream>
+#include "boost/random.hpp"
 
 class lassoTest:public ::testing::Test{
+public:
+  typedef boost::variate_generator< boost::mt19937&, boost::normal_distribution<> > dist_n;
+  typedef boost::variate_generator< boost::mt19937&, boost::uniform_real<> > dist_u;
 protected:
   virtual void SetUp() {
+    int n=40; //number of columns of A
+    int m=20; //number of rows of A
+    double p=1./n;
+    
+    //boost random crap
+    boost::mt19937 rng;
+    boost::normal_distribution<> nd(0.0, 1.0);
+    boost::uniform_real<> ud(0.0, 1.0);
+    dist_n var_nor(rng, nd);
+    dist_u var_uni(rng, ud);
+    rng.seed(0);
+    
+    //initialization as in LASSO sample
+    initialize_A(var_nor, n, m);
+    initialize_b(var_nor, var_uni, n, m, p);
 
+    L=new Lasso(A, b);
   }
   virtual void TearDown() {
-
+    delete L;
   }
-  lassoTest():L(){}
-  Lasso L;
+  lassoTest(){}
+  Eigen::MatrixXd A;
+  Eigen::VectorXd b;
+  Lasso *L;
 private:
+  boost::mt19937 rng;
+  void initialize_A(dist_n &var_nor, int n, int m);
+  void initialize_b(dist_n &var_nor, dist_u &var_uni, int n, int m, double p);
 };
+
+void lassoTest::initialize_A(dist_n &var_nor, int n, int m){
+/*  A = randn(m,n);
+    A = A*spdiags(1./sqrt(sum(A.^2))',0,n,n); % normalize columns
+ */ 
+  A.resize(m,n);
+  for(int i=0;i<A.cols();++i){
+    for(int j=0;j<A.rows();++j){
+      A(j,i)=var_nor();
+    }
+  }
+  //now normalize columns
+  for(int j=0;j<A.cols();++j){
+    double csum=0;
+    for(int i=0;i<A.rows();++i){
+      csum+=A(i,j)*A(i,j);
+    }
+    for(int i=0;i<A.rows();++i){
+      A(i,j)=A(i,j)/std::sqrt(csum);
+    }
+  }
+}
+void lassoTest::initialize_b(dist_n &var_nor, dist_u &var_uni, int n, int m, double p){
+  /*x0 = sprandn(n,1,p);
+  b = A*x0 + sqrt(0.001)*randn(m,1);*/
+  b.resize(m);
+  Eigen::VectorXd x0(n);
+  for(int i=0;i<n;++i){
+    if(var_uni()<p){
+      x0[i]=var_nor();
+    }else{
+      x0[i]=0.;
+    }
+  }
+  b=A*x0;
+  for(int i=0;i<m;++i){
+    b[i]+=std::sqrt(0.001)*var_nor();
+  }
+}
+
 TEST_F(lassoTest, Init) {
   ;//do nothing
 }
